3505affaac7939099188d522037ccacb920dd805
risteel <z3310z33104107@gmail.com>
2021-12-07 18:26:36 +0800

    o-enb send alarm

--- ntsimulator/deploy/o-ran-du/mqtt_sub.py
+++ ntsimulator/deploy/o-ran-du/mqtt_sub.py
@@ -5,7 +5,11 @@ import json
 import time
 import requests
 import subprocess
+from os.path import expanduser
 import paho.mqtt.client as mqtt
+from watchdog.events import *
+from watchdog.observers import Observer
+home = expanduser("~")
 
 # base = 'https://sdnc-web:8453'
 base = 'https://192.168.60.158:8453'
@@ -14,6 +18,43 @@ username = 'admin'
 password = 'Kp8bJ4SXszM0WXlhak3eHlcse2gAw84vaoGGmJvUy2U'
 requests.packages.urllib3.disable_warnings()
 
+
+class FileEventHandler(FileSystemEventHandler):
+    def __init__(self):
+        FileSystemEventHandler.__init__(self)
+
+    def on_moved(self, event):
+        if event.is_directory:
+            print("directory moved from {} to {}".format(event.src_path,event.dest_path))
+        else:
+            print("file moved from {} to {}".format(event.src_path,event.dest_path))
+            self.task(filName = event.dest_path)
+
+    def on_created(self, event):
+        if event.is_directory:
+            print("directory created:{}".format(event.src_path))
+        else:
+            print("file created:{}".format(event.src_path))
+
+    def on_deleted(self, event):
+        if event.is_directory:
+            print("directory deleted:{}".format(event.src_path))
+        else:
+            print("file deleted:{}".format(event.src_path))
+
+    def on_modified(self, event):
+        if event.is_directory:
+            print("directory modified:{}".format(event.src_path))
+        else:
+            print("file modified:{}".format(event.src_path))
+            if "alarm_config" in event.src_path:
+                with open(event.src_path, 'r') as fp:
+                    content = fp.read().strip().split('\n')
+                    print(content)
+                    send_message(content[0], content[1], content[2])
+
+    def task(self, filName):
+        print(filename)
 
 # REST to set event settings
 def configEventSettings(nfName, nfType, file_name):
@@ -69,43 +110,24 @@ def write_config(condition, object_str, severity):
     file.write(content)
     file.close()
 
-
-def on_connect(client, userdata, flags, rc):
-    print("Connected with result code "+str(rc))
-    client.subscribe("ORAN/MQTTYYDS")
-
-def on_message(client, userdata, msg):
-    send = False
-    content = msg.payload.decode('utf-8')
-    print(msg.topic+" "+ content)
+def send_message(condition, object_str, severity):
     send = True
     nfName = "o-eNB"
     if send is True:
-        if content == "1":
-            write_config("DoS", "Connlab", "MAJOR")
-            print("DoS")
-        elif content == "2":
-            write_config("Send SIB12", "Connlab", "CRITICAL")
-            print("SIB12")
-        elif content == "3":
-            write_config("DNS Attack", "Connlab", "MAJOR")
-            print("DNS Attack")
+        write_config(condition, object_str,severity)
+        print(condition)
         print("Set send: ", "o-eNB", configEventSettings(nfName, "o-ran-enb", "/event-settings_send.json"))
         time.sleep(1) 
         print("Set don't send: ", "o-eNB", configEventSettings(nfName, "o-ran-enb", "/event-settings_no_send.json"))
     # call api
 
-
-# main
-
-client = mqtt.Client()
-
-client.on_connect = on_connect
-
-client.on_message = on_message
-
-client.username_pw_set("connlab","LCCYCH507")
-
-client.connect("connmqttlab.jed.tw", 1883, 60)
-
-client.loop_forever()
+if __name__ == "__main__":
+    home = os.path.expanduser("~")
+    #print(home + "/file_test")
+    observer = Observer()
+    event_handler = FileEventHandler()
+    filePath = "/opt/dev/ntsim-ng/config/" #home + "/file_test"
+    observer.schedule(event_handler, filePath, True)
+    observer.start()
+    while True:
+        time.sleep(60)
